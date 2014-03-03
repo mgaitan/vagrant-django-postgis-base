c375c540d19823c1a82d5649fecadf6eaa1c889f
Natalia Tassin <gaitan@gmail.com>
2014-03-03 16:56:37 -0300

    wheels. some extra aptget things . update readme

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 vagrant-preciosa-base
 ======================
 
-A Vagrant box based on Ubuntu precise32, configured for Django development according to Torchbox's adopted practices and customized
-for the development of the `Preciosa <https://github.com/mgaitan/preciosa>`_ project. Things preinstalled beyond the base precise32 box include:
+A Vagrant box based on Ubuntu saucy32, configured for Django development according to Torchbox's adopted practices and customized
+for the development of the `Preciosa <https://github.com/mgaitan/preciosa>`_ project. Things preinstalled beyond the base saucy32 box include:
 
 * postgresql 9.1 (with locale fixed to create databases as UTF-8)
 * postgis 2.0.4
 * virtualenv and virtualenvwrapper
-* a pip download cache pre-seeded with Django, more or less current dependencies of Preciosa and various other common packages
+* a pip download cache pre-seeded with Django, more or less current dependencies of Preciosa and various other common packages in Wheel format (already compiled, i.e. faster than sources)
 * git (sometimes required for pip dependencies that aren't in PyPI)
+* a predefined GIS ready *"preciosa"* postgres database under the user 'dev' (password: dev)
 
 This box will successfully build from a vanilla precise32 base box, but using vagrant-django-base instead will skip some of the time-consuming initial setup.
+
+This box could be downloaded from http://lab.nqnwebs.com/preciosa-base-v0.1.box
 
 Build instructions
 ------------------
--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ PGSQL_VERSION=9.1
 POSTGIS_VERSION=2.0.4
 GEOS_VERSION=3.3.9
 
+
 # Need to fix locale so that Postgres creates databases in UTF-8
 cp -p /vagrant_data/etc-bash.bashrc /etc/bash.bashrc
 locale-gen ${LOCAL_LOCALE}
@@ -36,10 +37,13 @@ wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | pyth
 # Dependencies for image processing with Pillow (drop-in replacement for PIL)
 # supporting: jpeg, tiff, png, freetype, littlecms
 # (pip install pillow to get pillow itself, it is not in requirements.txt)
-apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
-#apt-get install -y imagemagick xsltproc libxml2-utils dblatex libcunit1 libcunit1-dev
-# Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
-apt-get install -y git
+apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev 
+# for lxml/scrapy
+apt-get install -y libxslt-dev libffi-dev
+#apt-get install -y imagemagick xsltproc libxml2-utils dblatex libcunit1 libcunit1-dev 
+# Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimlibxml2-dev libxslt-deves we need it for pip requirements that aren't in PyPI)
+apt-get install -y git mercurial
+
 
 # Postgresql
 if ! command -v psql; then
@@ -71,6 +75,18 @@ if ! command -v psql; then
     /etc/init.d/postgresql reload
 fi
 
+apt-get clean -y
+rm setuptools*.tar.gz
+
+# create gis ready db 'preciosa' under de user dev
+sudo -u postgres psql -c "CREATE ROLE dev LOGIN PASSWORD 'dev' SUPERUSER VALID UNTIL 'infinity';"
+sudo -u postgres createdb template_postgis
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/$PGSQL_VERSION/contrib/postgis-$POSTGIS_VERSION/postgis.sql
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/$PGSQL_VERSION/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/$PGSQL_VERSION/contrib/postgis_comments.sql
+sudo -u postgres psql -c "CREATE DATABASE preciosa WITH ENCODING='UTF8' OWNER=dev TEMPLATE=template_postgis LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' CONNECTION LIMIT=-1;"
+
+
 # virtualenv global setup
 if ! command -v pip; then
     easy_install -U pip
@@ -84,7 +100,7 @@ cp -p /vagrant_data/bashrc /home/vagrant/.bashrc
 
 if [[ ! -e /home/vagrant/.pip_download_cache ]]; then
     su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache && \
-        virtualenv /home/vagrant/yayforcaching && \
-        PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache /home/vagrant/yayforcaching/bin/pip install -r /vagrant_data/common_requirements.txt --pre && \
+        virtualenv /home/vagrant/yayforcaching && /home/vagrant/yayforcaching/bin/pip install wheel && \
+        PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache /home/vagrant/yayforcaching/bin/pip wheel --wheel-dir=/home/vagrant/.pip_download_cache -r /vagrant_data/common_requirements.txt scrapy && \
         rm -rf /home/vagrant/yayforcaching"
 fi
